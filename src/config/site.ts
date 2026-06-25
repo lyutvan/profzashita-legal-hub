bba650271373b891f496689daf6bf8f3d525ec2f
Ivan Ivanov <lyutvan05@mail.ru>
2026-06-25 16:15:24 +0300

    в

--- src/config/site.ts
+++ src/config/site.ts
@@ -14,7 +14,7 @@ export const SITE = {
   telegramUrl: "tg://resolve?phone=79168597654",
   maxUrl: "https://web.max.ru/?phone=79168597654",
   email: "info@prof-zashita.ru",
-  yandexMapsUrl: "https://yandex.ru/maps/?ll=37.536700%2C55.869700&z=16&text=%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0%2C%20%D1%83%D0%BB.%20%D0%94%D0%B5%D0%B3%D1%83%D0%BD%D0%B8%D0%BD%D1%81%D0%BA%D0%B0%D1%8F%2C%201%D0%BA2%2C%20%D0%BE%D1%84%D0%B8%D1%81%20303",
+  yandexMapsUrl: "https://yandex.ru/maps/org/profzashchita/244880896695?si=x1yev5uk531qz5gajfy6d5dwqc",
   address: {
     country: "RU",
     city: "Москва",
